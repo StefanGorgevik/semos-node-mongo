3d8c075c072cb3e5f7524d601a420737029e0904
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-11-19 20:28:45 +0100

    full rest CRUD + service config files

--- config/index.js
+++ config/index.js
@@ -1,21 +1,21 @@
-const fs = require('fs')
 
-var conf = null;
+const fs = require('fs');
+
 const configFile = './config.json';
+var conf = null;
 
 const getConfig = (section) => {
-    if (conf == null) {
-        if (fs.existsSync(configFile)) {
+    if(conf == null) {
+        if(fs.existsSync(configFile)){
             var json = fs.readFileSync(configFile);
-            conf = JSON.parse(json)
+            conf = JSON.parse(json);
         } else {
-            console.error("Could not find config file!")
+            console.error('Could not find config file!');
         }
     }
     return conf[section];
-};
+}
 
 module.exports = {
     getConfig
-};
-
+}
--- db/connection.js
+++ db/connection.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-const uri = 'mongodb+srv://{username}:{password}@{host}/{dbname}?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://{username}:{password}@{host}/{dbname}?retryWrites=true&w=majority';
 
 const initialize = (config) => {
-    mongoose.connect(parseCString(config),
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        .then(res => {
-            // console.log(res);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    mongoose.connect(
+        parseCString(config),
+        {useNewUrlParser: true, useUnifiedTopology: true}
+    )
+    .then(res => {
+        // console.log(res);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 }
 
 const parseCString = (config) => {
     var cs = uri.replace('{username}', config.username);
-    cs = uri.replace('{password}', config.password);
-    cs = uri.replace('{host}', config.host);
-    cs = uri.replace('{dbname}', config.dbname);
+    cs = cs.replace('{password}', config.password);
+    cs = cs.replace('{host}', config.host);
+    cs = cs.replace('{dbname}', config.dbname);
     return cs;
 }
 
-module.exports = {
-    initialize
-}
+module.exports = {initialize};
