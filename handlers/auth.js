fb98b1a2153693f9bbadd4275269ac87727bc4aa
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-11-27 16:50:57 +0100

    login register

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,31 +1,68 @@
-const modelUsers = require('../models/users');
-//https://www.npmjs.com/package/node-input-validator
-const validator  = require('node-input-validator');
-const validatorUsers = require('../validators/users')
 
+const mUsers = require('../models/users');
+const vUsers = require('../validators/users');
+//https://www.npmjs.com/package/node-input-validator
+var validator = require('node-input-validator');
+
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const config = require('../config/index.js');
 
 const register = (req, res) => {
-    var v = new validator.Validator(req.body, validatorUsers.createUser); //1-payload, 2-schema for validation
-    v.check() //returns a promise
+    var v = new validator.Validator(req.body, vUsers.createUser);
+    v.check()
     .then(matched => {
         if(matched) {
-            return modelUsers.createUser(req.body);
+            bcrypt.genSalt(10, function(err, salt) {
+                if(err){
+                    throw new Error(err);
+                    return;
+                }
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                    if(err){
+                        throw new Error(err);
+                        return;
+                    }
+                    return mUsers.createUser({...req.body, password: hash});
+                });
+            });
         } else {
-            throw new Error("Validation failed");
+            throw new Error('Validation failed');
         }
     })
-        .then(() => {
-            return res.status(201).send("User created")
-        })
-        .catch(err => {
-            console.log(err);
-            return res.status(500).send(v.errors);
-        })
-    }
+    .then(() => {
+        return res.status(201).send('ok');
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).send(v.errors);
+    });
+}
 
-    const login = (req, res) => {
-        return res.status(200).send("OK")
-    }
+const login = (req, res) => {
+    mUsers.getUserPasswordByEmail(req.body.email)
+    .then((data) => {
+        bcrypt.compare(req.body.password, data.password, function(err, rez) {
+            if(err){
+                return res.status(500).send('Could not compare password');
+            }
+            if(rez){
+                var tokenData = {
+                    id: rez._id,
+                    full_name: `${rez.first_name} ${rez.last_name}`,
+                    email: rez.email
+                };
+                var token = jwt.sign(tokenData, config.getConfig('jwt').key);
+                return res.status(200).send({jwt: token});
+            }
+            return res.status(404).send('not found');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).send('Could not get user');
+    });
+};
 
     const renew = (req, res) => {
         return res.status(200).send("OK")
